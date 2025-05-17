d1bcb92f2423b0d62c9149bb5d180b27eb3cfc1b
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-05-17 21:21:03 +0530

    Add GitHub Actions workflow for linting and DockerHub deployment

--- app/db.py
+++ app/db.py
@@ -10,10 +10,8 @@ NEO4J_USER = os.getenv("NEO4J_USER")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 # Create the Neo4j driver
-driver = GraphDatabase.driver(
-    NEO4J_URI,
-    auth=(NEO4J_USER, NEO4J_PASSWORD)
-)
+driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
+
 
 def get_driver():
     return driver
--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 
+
 class UserCreate(BaseModel):
     name: str
+
 
 class FriendRequest(BaseModel):
     user1: str
     user2: str
-
